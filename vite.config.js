02a9d4c895f1eaebe9c8c5ab9cb8a33e4dd55e78
Daniel <danielfloresd92@gmail.com>
2026-03-06 11:02:09 -0400

    remove config server on vite.comfig

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import fs from 'fs';
-import path from 'path';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'server.crt')),
-        },
-        strictPort: true,
-        host: true,
-        port: 5173
-    },
     plugins: [
         react(),
         basicSsl()
