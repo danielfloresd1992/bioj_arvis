cf531dfb62b1a7803b589a4b8074f5f1a551d503
Daniel <danielfloresd92@gmail.com>
2026-03-12 09:07:22 -0400

    Implementación de variables de entorno

--- src/network/user.js
+++ src/network/user.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+const URL = import.meta.env.VITE_API_URL;
 
+
+console.log(URL);
 
 export async function userIsExistAttendance(dni = '', urlImage, callback = () => { }) {
     if (dni === '') return null;
     try {
-        const response = await axios.post(`https://amazona365.ddns.net/api_jarvis/v1/user/attendance/machine/${dni}`, { imageReference: urlImage })
+        const response = await axios.post(`${URL}/user/attendance/machine/${dni}`, { imageReference: urlImage })
         callback(null, response);
     }
     catch (error) {
@@ -14,14 +17,16 @@ export async function userIsExistAttendance(dni = '', urlImage, callback = () =>
 }
 
 
-
 export async function save(dni, body) {
     if (!dni) return null;
     try {
-        const response = await axios.post(`https://amazona365.ddns.net/api_jarvis/v1/user/attendanc/${dni}`,);
+        const response = await axios.post(`${URL}/user/attendanc/${dni}`,);
         console.log(response);
     }
     catch (error) {
         console.log(error)
     }
 }
+
+
+//24 939 156
