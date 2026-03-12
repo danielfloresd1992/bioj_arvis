8dc5e2473c8a838361231cb42ba440cbd578fc27
Daniel <danielfloresd92@gmail.com>
2026-03-12 09:30:24 -0400

    sustitución de dns a iplocl en url de imagen

--- src/components/Section_data_user.jsx
+++ src/components/Section_data_user.jsx
@@ -2,18 +2,17 @@
 
 
 
-export default function ShowData({user=null}){
+export default function ShowData({ user = null }) {
 
 
     const changeDns = (dns) => {
-        if(!dns) return null;
+        if (!dns) return null;
+        return 'https://amazona365.ddns.net' + dns.split('https://72.68.60.254')[1]
+    };
 
-        return 'https://amazona365.ddns.net:3006' +dns.split('https://amazona365.ddns.net')[1]
-    }
 
-
-    return(
-        <div 
+    return (
+        <div
             style={{
                 width: '100%',
                 height: '100%',
@@ -21,23 +20,23 @@ export default function ShowData({user=null}){
         >
             {
                 user ?
-                    <div  style={{
-                            width: '100%',
-                            height: '100%',
-    
-                        }}
+                    <div style={{
+                        width: '100%',
+                        height: '100%',
+
+                    }}
                     >
-                        <img style={{width:'100%', /* nunca será más ancha que el padre */ height: '80%', objectFit: 'contain'}} src={changeDns(user.img)} alt="" />
+                        <img style={{ width: '100%', /* nunca será más ancha que el padre */ height: '80%', objectFit: 'contain' }} src={changeDns(user.img)} alt="" />
                         <div style={{
                             height: '20%',
-                            color:'#000'
+                            color: '#000'
                         }}>
-                            <p style={{fontSize: '1.1rem'}}>{user.name}</p>
-                            <p style={{fontSize: '1.1rem'}}>CI {user.dni}</p>
+                            <p style={{ fontSize: '1.1rem' }}>{user.name}</p>
+                            <p style={{ fontSize: '1.1rem' }}>CI {user.dni}</p>
                         </div>
                     </div>
-                :
-                null
+                    :
+                    null
             }
         </div>
     )
